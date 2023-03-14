8dd616f9d79008768c77e38e56c6343c520e795e
Gilangaditya19 <langzaditya21@gmail.com>
2023-03-14 15:31:23 +0700

    Upload File Praktikum 4

---
+++ Pertemuan4/Praktikum/webku/admin/club.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'template/sidebar.php';
+include_once 'template/topbar.php';
+?>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-4 text-gray-800">Daftar IT Club</h1>
+
+    </div>
+    <!-- /.container-fluid -->
+
+<?php
+include_once 'template/footer.php';
+?>
