bb0effc8c84d15a0d6e67861daa54fae97647ea8
Gilangaditya19 <langzaditya21@gmail.com>
2023-03-27 22:09:28 +0700

    Menambahkan file tugas dan hasil praktikum pertemuan ke 5

---
+++ Pertemuan5/gilangbelajar/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Hallo</h2>
+	<p>Selamat datang</p>
+</div>
+
+<?=template_footer()?>
